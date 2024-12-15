849e02ec0d7267f430e2c7ec69e0cebf451eba77
TANMAYAYUSHKABAAP <morningstudy35@gmail.com>
2024-12-15 18:06:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7355970298:AAEB36kySYQbAnhjFUq6kBOQ5MnUd1rnPGM"
+ADMIN_IDS = ["5181364124"]
+GROUP_ID = -1004510835358  # Replace with your group ID
+GROUP_LINK = "https://t.me/GODPREDICTION69"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
